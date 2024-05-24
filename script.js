fe6dd02ec1033fa2ccc12c2f8bd7f3208f69e40f
webploy99 <webploy99@gmail.com>
2024-05-24 13:48:43 +0700

    update commit

--- script.js
+++ script.js
@@ -28,3 +28,26 @@ function OpenNav(){
     document.documentElement.scrollTop = 0;
     }
 
+    // slide show
+    document.addEventListener('DOMContentLoaded', function () {
+        var splide = new Splide('.splide', {
+            perPage: 1,
+            gap: '15px',
+            type: 'loop',
+            autoplay: true,
+            interval: 2000,
+            snap: true,
+            breakpoints: {
+                448: {
+                    perPage: 1
+                },
+                768: {
+                    perPage: 2
+                }
+            }
+            // start: 2,
+            // pagination: false,
+        });
+        splide.mount();
+    });
+
